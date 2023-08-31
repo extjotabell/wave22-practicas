500d4da57fd87e33a2773afb7c758e39a03b46d0
Valentina Iara Gottelli <valentina.gottelli@mercadolibre.com>
2023-08-31 16:44:58 -0300

    Ejercicios POO java 2

---
+++ 2. java/poo-p1/src/main/java/ej2/NoPerecedero.java
@@ -0,0 +1,27 @@
+package ej2;
+
+public class NoPerecedero extends Producto{
+    private String tipo;
+
+    public NoPerecedero(String name, double price, String tipo) {
+        super(name, price);
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "NoPerecedero{" +
+                "name='" + super.getName() + '\'' +
+                ", price=" + super.getPrice() +
+                ", tipo='" + tipo + '\'' +
+                '}';
+    }
+}
