9b092ccfbb8645d3e78b5714ef5421e4ac3466ea
juliojalvare <juliojoel.alvarez@mercadolibre.com>
2023-08-31 13:42:40 -0300

    Add excercise one of POO

---
+++ 2. java/ejercicio1-poo/src/domain/Persona.java
@@ -0,0 +1,54 @@
+package domain;
+
+public class Persona {
+    String nombre;
+    int edad;
+    String dni;
+    double peso;
+    double altura;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, int edad, String dni) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+    }
+
+    public Persona(String nombre, int edad, String dni, double peso, double altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public int calcularIMC(){
+        double IMC = peso / (Math.pow(altura, 2));
+
+        if(IMC < 20) {
+            return -1;
+        }
+        else if(IMC > 25){
+            return 1;
+        }
+        return 0;
+    }
+
+
+    public boolean esMayor() {
+        return edad >= 18;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", dni='" + dni + '\'' +
+                ", peso=" + peso +
+                ", altura=" + altura +
+                '}';
+    }
+}
