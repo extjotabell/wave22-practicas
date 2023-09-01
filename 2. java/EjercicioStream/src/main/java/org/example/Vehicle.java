d3fa7cdf3d3223e53945ba295c0413eb1db09426
LucianaPm <luciana.pio@mercadolibre.com>
2023-09-01 17:45:33 -0300

    Add streams practice

---
+++ 2. java/EjercicioStream/src/main/java/org/example/Vehicle.java
@@ -0,0 +1,46 @@
+package org.example;
+
+public class Vehicle {
+    private String model;
+    private String brand;
+    private double cost;
+
+    public Vehicle(String model, String brand, int cost) {
+        this.model = model;
+        this.brand = brand;
+        this.cost = cost;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "\n Vehicle{" +
+                "model='" + model + '\'' +
+                ", brand='" + brand + '\'' +
+                ", cost=" + cost +
+                '}';
+    }
+}
