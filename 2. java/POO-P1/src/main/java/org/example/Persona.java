f43ec53ecbe085cc5190e621b10182af6996fe76
clemos <camila.lemos@mercadolibre.com>
2023-09-02 18:09:43 -0300

    agrego ejercicio POO-P1

---
+++ 2. java/POO-P1/src/main/java/org/example/Persona.java
@@ -0,0 +1,47 @@
+package org.example;
+
+public class Persona {
+    private String nombre;
+    private String dni;
+    private int edad;
+    private double peso;
+    private double altura;
+
+    public Persona() {}
+    public Persona(String nombre, String dni, int edad) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.edad = edad;
+    }
+    public Persona(String nombre, String dni, int edad, double peso, double altura) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.edad = edad;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public double calcularIMC() {
+        double resultadoIMC = peso/Math.pow(altura, 2);
+        if (resultadoIMC >= 20 && resultadoIMC <= 25 ) {
+            return 0;
+        }
+        if (resultadoIMC < 20) {
+            return -1;
+        }
+        return 1;
+    }
+
+    public boolean esMayorDeEdad() {
+        return edad >= 18;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre='" + nombre + '\'' +
+                ", Dni='" + dni + '\'' +
+                ", Edad=" + edad +
+                ", Peso=" + peso +
+                ", Altura=" + altura;
+    }
+}
