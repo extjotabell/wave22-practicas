5e9a5301cf73342c1c4ca7160d40295f706548b2
clemos <camila.lemos@mercadolibre.com>
2023-09-02 18:23:39 -0300

    agrego ejercicio POO-P2

---
+++ 2. java/POO-P2/src/main/java/org/example/Perecedero.java
@@ -0,0 +1,46 @@
+package org.example;
+
+public class Perecedero extends Producto {
+
+    private int diasPorCaducar;
+
+    public int getDiasPorCaducar() {
+        return diasPorCaducar;
+    }
+
+    public void setDiasPorCaducar(int diasPorCaducar) {
+        this.diasPorCaducar = diasPorCaducar;
+    }
+
+    public Perecedero(String nombre, double precio, int diasPorCaducar) {
+        super(nombre, precio);
+        this.diasPorCaducar = diasPorCaducar;
+    }
+
+    @Override
+    public String toString() {
+        return "Perecedero{" +
+                "diasPorCaducar=" + diasPorCaducar +
+                '}';
+    }
+
+    @Override
+    public double calcular(int cantidadDeProductos) {
+        return super.calcular(cantidadDeProductos) / reducirSegunCaducidad();
+    }
+
+    private int reducirSegunCaducidad() {
+        switch (this.diasPorCaducar) {
+            case 1:
+                return 4;
+            case 2:
+                return 3;
+            case 3:
+                return 2;
+            default:
+                return 1;
+        }
+    }
+
+}
+
