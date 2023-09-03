772973dec9adb34f9c98c552b2cfdffff7216470
Randy Mendoza <randy.mendoza@mercadolibre.com>
2023-09-02 22:06:52 -0300

    agregado de Part 1 de ejercicio Integrador

---
+++ 2. java/Ejercicios Integradores - P1 -PG/Supermercado/src/main/java/Main.java
@@ -0,0 +1,37 @@
+import model.Cliente;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente(3123,"Randy","Mendoza");
+        Cliente cliente2 = new Cliente(64563,"Belen","Velasquez");
+        Cliente cliente3 = new Cliente(43234,"Camila","rodriguez");
+
+        Map<Integer,Cliente> clientes = new HashMap<>();
+        clientes.put(cliente1.getDni(),cliente1);
+        clientes.put(cliente2.getDni(),cliente2);
+        clientes.put(cliente3.getDni(),cliente3);
+
+        clientes.forEach((key,value) -> System.out.println(value));
+
+        // eliminamos uno e iteramos de nuevo
+        clientes.remove(3123);
+        clientes.forEach((key,value) -> System.out.println(value));
+
+        findClientByDni(clientes);
+
+
+    }
+    private static void findClientByDni(Map<Integer, Cliente> clientes){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese el dni del cliente que desea buscar: ");
+        int dni = input.nextInt();
+
+        if(clientes.containsKey(dni)){
+            System.out.println(clientes.get(dni));
+        }else{
+            System.out.println("El cliente no se encuentra en la BD");
+        }
+    }
+}
---
+++ 2. java/Ejercicios Integradores - P1 -PG/Supermercado/src/main/java/model/Factura.java
@@ -0,0 +1,44 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Factura {
+    private Cliente cliente;
+    private List<Item> items = new ArrayList<>();
+    private double totalCompra;
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public double getTotalCompra() {
+        return totalCompra;
+    }
+
+    public void setTotalCompra(double totalCompra) {
+        this.totalCompra = totalCompra;
+    }
+
+    public Factura(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Factura(Cliente cliente, List<Item> items, double totalCompra) {
+        this.cliente = cliente;
+        this.items = items;
+        this.totalCompra = totalCompra;
+    }
+}
