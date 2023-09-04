c6a14c3c2a0563b48151760272ed81df6868ecf9
telorza <tatiana.elorza@mercadolibre.com>
2023-09-04 16:50:45 -0300

    add ejercicio SaveTheRopa y comienzo Dakar

---
+++ 2. java/Dakar/src/main/java/org/example/Carrera.java
@@ -0,0 +1,22 @@
+package org.example;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carrera {
+
+    private Double distancia;
+    private Integer premioEnDolares;
+    private String nombre;
+    private Integer cantidadDeVehiculosPermitidos;
+    private List<Vehiculo> listaDeVehiculos = new ArrayList<>();
+
+    public void darDeAltaAuto(){
+
+    }
+
+    public void darDeAltaMoto(){
+
+    }
+}
---
+++ 2. java/Dakar/src/main/java/org/example/Vehiculo.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Vehiculo {
+    private Double velocidad;
+    private Double aceleracion;
+    private Integer anguloDeGiro;
+    private String patente;
+    private Double peso;
+    private Integer ruedas;
+}
