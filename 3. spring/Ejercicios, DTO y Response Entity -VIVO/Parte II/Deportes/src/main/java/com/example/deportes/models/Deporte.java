da8d8e7d1680ce5b1941b759e1b18eabfd9c0268
clemos <camila.lemos@mercadolibre.com>
2023-09-06 16:43:49 -0300

    Agrego ejercicio Deporte

---
+++ 3. spring/Ejercicios, DTO y Response Entity -VIVO/Parte II/Deportes/src/main/java/com/example/deportes/models/Deporte.java
@@ -0,0 +1,12 @@
+package com.example.deportes.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Deporte {
+
+    private String nombre;
+    private int nivel;
+}
---
+++ 3. spring/Ejercicios, DTO y Response Entity -VIVO/Parte II/Deportes/src/main/java/com/example/deportes/models/Persona.java
@@ -0,0 +1,14 @@
+package com.example.deportes.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Persona {
+
+    private String nombre;
+    private String apellido;
+    private int edad;
+
+}
