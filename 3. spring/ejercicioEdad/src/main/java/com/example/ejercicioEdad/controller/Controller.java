2b20d5296d9f56ebd87e6db1e26721af468a7754
telorza <tatiana.elorza@mercadolibre.com>
2023-09-06 12:16:47 -0300

    add ejercicio anios

---
+++ 3. spring/ejercicioEdad/src/main/java/com/example/ejercicioEdad/controller/Controller.java
@@ -0,0 +1,17 @@
+package com.example.ejercicioEdad.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+@RestController
+public class Controller {
+    @GetMapping("/{dia}/{mes}/{anio}")
+    public Integer getEdad(@PathVariable Integer dia, @PathVariable Integer mes, @PathVariable Integer anio) {
+        LocalDate fecha = LocalDate.of(anio, mes, dia);
+        return Period.between(fecha, LocalDate.now()).getYears();
+    }
+}
