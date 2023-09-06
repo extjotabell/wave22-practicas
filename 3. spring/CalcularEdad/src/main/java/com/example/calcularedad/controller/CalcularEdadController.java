68ab0a30ef3140c20e2b4efbeb4856e28462386a
Santiago Langer <santiago.langer@mercadolibre.com>
2023-09-06 11:50:25 -0300

    Add files via upload

---
+++ 3. spring/CalcularEdad/src/main/java/com/example/calcularedad/controller/CalcularEdadController.java
@@ -0,0 +1,21 @@
+package com.example.calcularedad.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+@RequestMapping("/")
+@RestController
+public class CalcularEdadController {
+    @GetMapping("{day}/{month}/{year}")
+    public int obtenerFecha(@PathVariable int day, @PathVariable int month, @PathVariable int year){
+        LocalDate fechaDeEntrada = LocalDate.of(year, month, day);
+        System.out.println("fechaDeEntrada");
+        Period periodo = Period.between(fechaDeEntrada, LocalDate.now());
+        return periodo.getYears();
+    }
+}
