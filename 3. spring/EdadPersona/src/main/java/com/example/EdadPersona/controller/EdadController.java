7b23caeb72526a6dbbaa1b7950207e443b4edc09
magui-cagna <mariaagostina.cagna@mercadolibre.com>
2023-09-06 12:47:57 -0400

    subiendo edadpersona

--- 3. spring/EdadPersona/src/main/java/com/example/EdadPersona/controller/EdadController.java
+++ 3. spring/EdadPersona/src/main/java/com/example/EdadPersona/controller/EdadController.java
@@ -1,2 +1,20 @@
-package com.example.EdadPersona.controller;public class EdadController {
+package com.example.EdadPersona.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+@RestController
+public class EdadController {
+    @GetMapping("fecha/{dia}/{mes}/{anio}")
+    public int calcularEdad(@PathVariable int dia, @PathVariable int mes, @PathVariable int anio){
+        LocalDate nacimiento = LocalDate.of(anio, mes, dia);
+        LocalDate hoy = LocalDate.now();
+        Period dif = Period.between(nacimiento, hoy);
+
+        return dif.getYears();
+    }
 }
