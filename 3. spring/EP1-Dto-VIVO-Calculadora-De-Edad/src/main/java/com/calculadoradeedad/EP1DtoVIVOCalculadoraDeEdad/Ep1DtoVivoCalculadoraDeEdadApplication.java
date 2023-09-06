208014561e846593327bd177951161ceb58a326b
Agustin Tarda <atarda@ar000hl6hwtg2m1.lan>
2023-09-06 11:48:58 -0300

    Added the Ejercicios Prácticos - Parte 1 from Ejercicios, DTO y Response Entity - VIVO

---
+++ 3. spring/EP1-Dto-VIVO-Calculadora-De-Edad/src/main/java/com/calculadoradeedad/EP1DtoVIVOCalculadoraDeEdad/Ep1DtoVivoCalculadoraDeEdadApplication.java
@@ -0,0 +1,13 @@
+package com.calculadoradeedad.EP1DtoVIVOCalculadoraDeEdad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ep1DtoVivoCalculadoraDeEdadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ep1DtoVivoCalculadoraDeEdadApplication.class, args);
+	}
+
+}
---
+++ 3. spring/EP1-Dto-VIVO-Calculadora-De-Edad/src/test/java/com/calculadoradeedad/EP1DtoVIVOCalculadoraDeEdad/Ep1DtoVivoCalculadoraDeEdadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculadoradeedad.EP1DtoVIVOCalculadoraDeEdad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ep1DtoVivoCalculadoraDeEdadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
