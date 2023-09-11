9d336db7031e3926b32d5bd907df3f7c8d715dcf
iroldan <iaragrisel.roldan@mercadolibre.com>
2023-09-10 22:32:58 -0300

    Ejercicio calculadora de edad

---
+++ 3. spring/age-calculator/src/main/java/com/example/agecalculator/AgeCalculatorController.java
@@ -0,0 +1,24 @@
+package com.example.agecalculator;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+@RestController
+@RequestMapping("/age-calculator")
+public class AgeCalculatorController {
+    @GetMapping("/{day}/{month}/{year}")
+    public int calculateAge(@PathVariable int day, @PathVariable int month, @PathVariable int year){
+        LocalDate birthdate = LocalDate.of(year, month, day);
+        LocalDate currentDate = LocalDate.now();
+
+        if (birthdate.isAfter(currentDate))
+            return 0;
+
+        return Period.between(birthdate, currentDate).getYears();
+    }
+}
