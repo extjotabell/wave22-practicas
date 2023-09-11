f91892506da96a51c6ad17e45deb32a7f576be12
clemos <camila.lemos@mercadolibre.com>
2023-09-11 15:43:43 -0300

    Agrego ejercicios

---
+++ 3. spring/DTO y Reesponse Entity  P2 - PG/covid-19/src/main/java/com/example/covid19/Entity/Persona.java
@@ -0,0 +1,10 @@
+package com.example.covid19.Entity;
+
+@AllArgsConstructor
+@Data
+public class Persona {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private int edad;
+}
---
+++ 3. spring/DTO y Reesponse Entity  P2 - PG/covid-19/src/main/java/com/example/covid19/Entity/Sintoma.java
@@ -0,0 +1,9 @@
+package com.example.covid19.Entity;
+
+@AllArgsConstructor
+@Data
+public class Sintoma {
+    private String codigo;
+    private String nombre;
+    private int nivel_de_gravedad;
+}
