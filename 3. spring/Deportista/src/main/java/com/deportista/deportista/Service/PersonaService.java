9688640136e5cad08cfe1ecba3c14e9b65a9e62e
Solange Recayte <solange.recayte@mercadolibre.com>
2023-09-12 15:13:17 -0300

    se agrego modelMapper

--- 3. spring/Deportista/src/main/java/com/deportista/deportista/Service/PersonaService.java
+++ 3. spring/Deportista/src/main/java/com/deportista/deportista/Service/PersonaService.java
@@ -5,6 +5,7 @@ import com.deportista.deportista.DTO.PersonaDTO;
 import com.deportista.deportista.Models.Deporte;
 import com.deportista.deportista.Models.Persona;
 import com.deportista.deportista.Repository.IPersonaRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,24 +17,27 @@ public class PersonaService implements IPersonaService{
 
     @Autowired
     IPersonaRepository _repo;
+
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public List<DeporteDTO> GetAllDeporte() {
         List<Deporte> deporteList= _repo.GetAllDeporte();
-        return deporteList.stream().map(x->new DeporteDTO(x.getNombre(),x.getNivel()))
+        return deporteList.stream().map(deporte -> mapper.map(deporte, DeporteDTO.class))
                                    .collect(Collectors.toList());
     }
 
     @Override
     public DeporteDTO GetByNameDeporte(String name) {
         Deporte deporte= _repo.GetByNameDeporte(name);
-        return new DeporteDTO(deporte.getNombre(),deporte.getNivel());
-
+        return mapper.map(deporte, DeporteDTO.class);
     }
 
     @Override
     public List<PersonaDTO> GetPersona() {
        List<Persona> personaList= _repo.GetPersona();
-       return personaList.stream().map(x->new PersonaDTO(x.getNombre(),x.getApellido(),x.getDeporte().getNombre()))
-                                  .collect(Collectors.toList());
+        return personaList.stream().map(persona -> mapper.map(persona, PersonaDTO.class))
+                                   .collect(Collectors.toList());
     }
 }
