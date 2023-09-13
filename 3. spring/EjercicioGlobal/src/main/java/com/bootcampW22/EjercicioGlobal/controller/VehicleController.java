0a615815979ba976f5a41782e5620a4ab7a72cdb
Rodrigo Baliani <rodrigo.baliani@mercadolibre.com>
2023-09-13 17:23:55 -0300

    Error 400 para vehicles/dimensions

--- 3. spring/EjercicioGlobal/src/main/java/com/bootcampW22/EjercicioGlobal/controller/VehicleController.java
+++ 3. spring/EjercicioGlobal/src/main/java/com/bootcampW22/EjercicioGlobal/controller/VehicleController.java
@@ -1,9 +1,10 @@
 package com.bootcampW22.EjercicioGlobal.controller;
 
 import com.bootcampW22.EjercicioGlobal.dto.VehicleDto;
-import com.bootcampW22.EjercicioGlobal.entity.Vehicle;
+import com.bootcampW22.EjercicioGlobal.exception.BadRequestException;
 import com.bootcampW22.EjercicioGlobal.service.IVehicleService;
 import com.bootcampW22.EjercicioGlobal.service.VehicleServiceImpl;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 @RestController
 public class VehicleController {
@@ -27,9 +29,13 @@ public class VehicleController {
     }
 
     @GetMapping("/vehicles/dimensions")
-    public ResponseEntity<List<VehicleDto>> getVehiclesByHeightAndWidth(@RequestParam String height, @RequestParam String width) {
-        String heightValues[] = height.split("-");
-        String widthValues[] = width.split("-");
-        return new ResponseEntity<List<VehicleDto>>(vehicleService.findVehiclesByHeightAndWidth(Double.parseDouble(heightValues[0]), Double.parseDouble(heightValues[1]), Double.parseDouble(widthValues[0]), Double.parseDouble(widthValues[1])), HttpStatus.OK);
+    public ResponseEntity<List<VehicleDto>> getVehiclesByHeightAndWidth(@RequestParam String height, @RequestParam String width) throws BadRequestException {
+        Pattern pattern = Pattern.compile("^\\d+\\-\\d+$");
+        //TODO: refactorizar pattern para que soporte decimales, por el momento solo soporta enteros
+        if(!pattern.matcher(height).matches() || !pattern.matcher(width).matches())
+            throw new BadRequestException("Formato de números incorrecto");
+        String[] heightValues = height.split("-");
+        String[] widthValues = width.split("-");
+        return new ResponseEntity<>(vehicleService.findVehiclesByHeightAndWidth(Double.parseDouble(heightValues[0]), Double.parseDouble(heightValues[1]), Double.parseDouble(widthValues[0]), Double.parseDouble(widthValues[1])), HttpStatus.OK);
     }
 }
---
+++ 3. spring/EjercicioGlobal/src/main/java/com/bootcampW22/EjercicioGlobal/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.bootcampW22.EjercicioGlobal.exception;
+
+public class BadRequestException extends Exception {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
