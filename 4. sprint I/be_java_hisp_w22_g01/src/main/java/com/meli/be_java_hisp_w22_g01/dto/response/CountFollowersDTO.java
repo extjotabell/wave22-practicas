bd7c1987f76a02b5b8e0d93e7fef9073053ad025
belensantochi <belen.santochi@mercadolibre.com>
2023-09-14 16:13:41 -0300

    modifico respuesta en feature US_0002

--- 4. sprint I/be_java_hisp_w22_g01/src/main/java/com/meli/be_java_hisp_w22_g01/dto/response/CountFollowersDTO.java
+++ 4. sprint I/be_java_hisp_w22_g01/src/main/java/com/meli/be_java_hisp_w22_g01/dto/response/CountFollowersDTO.java
@@ -8,5 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CountFollowersDTO {
+    private int user_id;
+    private String user_name;
     private int followers;
 }
