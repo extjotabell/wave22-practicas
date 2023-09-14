65af31bce215a69d125d802ea218029b77a5180c
LucianaPm <luciana.pio@mercadolibre.com>
2023-09-14 11:39:09 -0300

    Create project social meli

---
+++ 4. sprint I/be_java_hisp_w22_g4/src/main/java/bootcamp/socialMeli/BeJavaHispW22G4Application.java
@@ -0,0 +1,13 @@
+package bootcamp.socialMeli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeJavaHispW22G4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeJavaHispW22G4Application.class, args);
+	}
+
+}
