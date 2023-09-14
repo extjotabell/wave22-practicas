a90b92451a9e61fa9bc56f559929beb204ba3863
facujaneiro <facundo.janeiro@mercadolibre.com>
2023-09-14 11:43:00 -0300

    First commit

---
+++ 4. sprint I/be_java_hisp_w22_g05/src/test/java/com/example/be_java_hisp_w22_g05/BeJavaHispW22G05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be_java_hisp_w22_g05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeJavaHispW22G05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
