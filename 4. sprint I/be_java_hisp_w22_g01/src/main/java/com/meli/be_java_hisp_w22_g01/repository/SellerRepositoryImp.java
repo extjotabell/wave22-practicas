e24b6d03472bfcb5ebd8b42f01873ba4a828541d
gonzaloespanah <gonzalo.espana@mercadolibre.cl>
2023-09-14 16:27:35 -0300

    add fake data seller

--- 4. sprint I/be_java_hisp_w22_g01/src/main/java/com/meli/be_java_hisp_w22_g01/repository/SellerRepositoryImp.java
+++ 4. sprint I/be_java_hisp_w22_g01/src/main/java/com/meli/be_java_hisp_w22_g01/repository/SellerRepositoryImp.java
@@ -1,6 +1,8 @@
 package com.meli.be_java_hisp_w22_g01.repository;
 
+import com.meli.be_java_hisp_w22_g01.entity.Post;
 import com.meli.be_java_hisp_w22_g01.entity.Seller;
+import com.meli.be_java_hisp_w22_g01.entity.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -11,6 +13,43 @@ public class SellerRepositoryImp implements ISellerRepository{
 
     private List<Seller> sellerList = new ArrayList<>();
     private UserRepositoryImp userRepo;
+
+    public SellerRepositoryImp() {
+
+        IPostRepository postRepository = new PostRepositoryImp();
+
+        // Post pertenecientes a los vendedores
+        List<Post> posts1 = new ArrayList<>();
+        posts1.add(postRepository.getPostById(1));
+
+        List<Post> posts2 = new ArrayList<>();
+        posts2.add(postRepository.getPostById(2));
+
+        List<Post> posts3 = new ArrayList<>();
+        posts3.add(postRepository.getPostById(1));
+        posts3.add(postRepository.getPostById(2));
+
+        //Seguidores de los vendedores
+        List<User> followers1 = new ArrayList<>();
+        List<User> followers2 = new ArrayList<>();
+
+        Seller seller1 = new Seller(posts1, followers1);
+        seller1.setUser_id(1);
+        seller1.setUser_name("Rick Harrison");
+
+        Seller seller2 = new Seller(posts2, followers2);
+        seller2.setUser_id(2);
+        seller2.setUser_name("Zadie Smith");
+
+        Seller seller3 = new Seller(posts3, followers2);
+        seller3.setUser_id(3);
+        seller3.setUser_name("Comefulanito");
+
+        this.sellerList.add(seller1);
+        this.sellerList.add(seller2);
+        this.sellerList.add(seller3);
+    }
+
     @Override
     public void save(Seller seller) {
         sellerList.add(seller);
