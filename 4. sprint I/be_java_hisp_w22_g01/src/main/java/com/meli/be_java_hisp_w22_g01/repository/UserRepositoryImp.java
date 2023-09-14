05efa68db0fe01a5419e6afc27085cc0ca3f57c1
Emmanuel F Paiva <emmanuel.paiva@mercadolibre.com>
2023-09-14 13:33:43 -0300

    Exceptions

--- 4. sprint I/be_java_hisp_w22_g01/src/main/java/com/meli/be_java_hisp_w22_g01/repository/UserRepositoryImp.java
+++ 4. sprint I/be_java_hisp_w22_g01/src/main/java/com/meli/be_java_hisp_w22_g01/repository/UserRepositoryImp.java
@@ -1,10 +1,30 @@
 package com.meli.be_java_hisp_w22_g01.repository;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.meli.be_java_hisp_w22_g01.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class UserRepositoryImp {
+import java.util.ArrayList;
+import java.util.List;
 
+@Repository
+public class UserRepositoryImp implements IUserRepository{
+
+    private List<User> list_of_users = new ArrayList<>();
+
+    @Override
+    public void save(User user) {
+
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+
+    @Override
+    public User findById(int user_id) {
+        return null;
+    }
 }
