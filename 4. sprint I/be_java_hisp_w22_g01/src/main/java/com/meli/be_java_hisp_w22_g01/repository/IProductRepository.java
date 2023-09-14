76501e26b8f75e316d5ed36590f891700b8b0620
gonzaloespanah <gonzalo.espana@mercadolibre.cl>
2023-09-14 15:16:02 -0300

    add ProductRepositoryImp

--- 4. sprint I/be_java_hisp_w22_g01/src/main/java/com/meli/be_java_hisp_w22_g01/repository/IProductRepository.java
+++ 4. sprint I/be_java_hisp_w22_g01/src/main/java/com/meli/be_java_hisp_w22_g01/repository/IProductRepository.java
@@ -1,4 +1,9 @@
 package com.meli.be_java_hisp_w22_g01.repository;
 
+import com.meli.be_java_hisp_w22_g01.entity.Product;
+
+import java.util.List;
+
 public interface IProductRepository {
+    public List<Product> getAllProducts();
 }
