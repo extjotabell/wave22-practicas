a1fb64ca4fb87526ffb66b2c917d5f6d6534b617
facujaneiro <facundo.janeiro@mercadolibre.com>
2023-09-14 11:28:20 -0300

    first commit

---
+++ 4. sprint I/MeliSocial/src/main/java/com/example/melisocial/MeliSocialApplication.java
@@ -0,0 +1,13 @@
+package com.example.melisocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeliSocialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeliSocialApplication.class, args);
+    }
+
+}
---
+++ 4. sprint I/MeliSocial/src/test/java/com/example/melisocial/MeliSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.melisocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeliSocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
