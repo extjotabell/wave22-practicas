7e178dd7cd01c58b1977ea86f3fa3ecb198165aa
Emmanuel F Paiva <emmanuel.paiva@mercadolibre.com>
2023-09-14 16:01:01 -0300

    dependenncias

--- 4. sprint I/be_java_hisp_w22_g01/src/main/java/com/meli/be_java_hisp_w22_g01/repository/UserRepositoryImp.java
+++ 4. sprint I/be_java_hisp_w22_g01/src/main/java/com/meli/be_java_hisp_w22_g01/repository/UserRepositoryImp.java
@@ -1,11 +1,16 @@
 package com.meli.be_java_hisp_w22_g01.repository;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.meli.be_java_hisp_w22_g01.entity.User;
+import com.meli.be_java_hisp_w22_g01.exceptions.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
-public class UserRepositoryImp {
+public class UserRepositoryImp implements IUserRepository{
 
     private List<User> list_of_users = new ArrayList<>();
 
