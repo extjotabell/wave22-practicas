959584d25e0e241ce7c6e3ed9516fe78119cf392
Emmanuel F Paiva <emmanuel.paiva@mercadolibre.com>
2023-09-14 14:58:26 -0300

    endpoint punto 3

--- 4. sprint I/be_java_hisp_w22_g01/src/main/java/com/meli/be_java_hisp_w22_g01/controller/UserController.java
+++ 4. sprint I/be_java_hisp_w22_g01/src/main/java/com/meli/be_java_hisp_w22_g01/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.meli.be_java_hisp_w22_g01.controller;
 
+import com.meli.be_java_hisp_w22_g01.service.IUserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    IUserService userService;
+
     @GetMapping("/{userId}/followers/list")
-    public ResponseEntity<?> followersList(){
-        return null;
+    public ResponseEntity<?> followersList(@PathVariable int userId){
+        return new ResponseEntity<>(userService.FollowersList(userId), HttpStatus.OK);
     }
 }
