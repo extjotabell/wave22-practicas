864d8989fcdd58bfc116086faa4a71fb35bf21ba
juliojalvare <juliojoel.alvarez@mercadolibre.com>
2023-09-15 12:58:27 -0300

    Add `UserMapper`

--- 4. sprint I/be_java_hisp_w22_g02/src/main/java/com/example/be_java_hisp_w22_g02/mapper/UserMapper.java
+++ 4. sprint I/be_java_hisp_w22_g02/src/main/java/com/example/be_java_hisp_w22_g02/mapper/UserMapper.java
@@ -1,11 +1,11 @@
 package com.example.be_java_hisp_w22_g02.mapper;
 
-import com.example.be_java_hisp_w22_g02.dto.response.UserDTO;
+import com.example.be_java_hisp_w22_g02.dto.response.UserDto;
 import com.example.be_java_hisp_w22_g02.entity.User;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    UserDTO toDto (User user);
-    User toEntity(UserDTO userDTO);
+    UserDto toDto (User user);
+    User toEntity(UserDto userDTO);
 }
