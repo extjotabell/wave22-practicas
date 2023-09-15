945f2e605b7291a95288b2f25a464ae70ed1044c
juliojalvare <juliojoel.alvarez@mercadolibre.com>
2023-09-15 12:57:20 -0300

    Fix `UserDto`

--- 4. sprint I/be_java_hisp_w22_g02/src/main/java/com/example/be_java_hisp_w22_g02/dto/response/UserDto.java
+++ 4. sprint I/be_java_hisp_w22_g02/src/main/java/com/example/be_java_hisp_w22_g02/dto/response/UserDto.java
@@ -3,6 +3,7 @@ package com.example.be_java_hisp_w22_g02.dto.response;
 import com.example.be_java_hisp_w22_g02.entity.Post;
 import com.example.be_java_hisp_w22_g02.entity.User;
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 @Data
 public class UserDto {
 
