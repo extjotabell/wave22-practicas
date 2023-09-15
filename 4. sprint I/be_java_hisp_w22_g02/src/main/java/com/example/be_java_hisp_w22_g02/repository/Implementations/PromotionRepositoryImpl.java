6b9855be96733f1a58665e477af62dcabad4f88a
juliojalvare <juliojoel.alvarez@mercadolibre.com>
2023-09-15 11:39:54 -0300

    Refactor `PromotionRepositoryImpl`

--- 4. sprint I/be_java_hisp_w22_g02/src/main/java/com/example/be_java_hisp_w22_g02/repository/Implementations/PromotionRepositoryImpl.java
+++ 4. sprint I/be_java_hisp_w22_g02/src/main/java/com/example/be_java_hisp_w22_g02/repository/Implementations/PromotionRepositoryImpl.java
@@ -1,14 +1,10 @@
 package com.example.be_java_hisp_w22_g02.repository.Implementations;
 
-import com.example.be_java_hisp_w22_g02.entity.Promotion;
 import com.example.be_java_hisp_w22_g02.repository.Interfaces.IPromotionRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Repository
 public class PromotionRepositoryImpl implements IPromotionRepository {
-    private final Map<Long, Promotion> dbPromotion = new HashMap<>();
 
 }
