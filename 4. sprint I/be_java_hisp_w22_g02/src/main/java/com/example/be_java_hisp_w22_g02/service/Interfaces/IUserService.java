2d5bc7b69fefc14cdef32a9180c720b2a413bd30
juliojalvare <juliojoel.alvarez@mercadolibre.com>
2023-09-15 12:55:07 -0300

    Fix `getFollowedUsersById` data type

--- 4. sprint I/be_java_hisp_w22_g02/src/main/java/com/example/be_java_hisp_w22_g02/service/Interfaces/IUserService.java
+++ 4. sprint I/be_java_hisp_w22_g02/src/main/java/com/example/be_java_hisp_w22_g02/service/Interfaces/IUserService.java
@@ -1,14 +1,13 @@
 package com.example.be_java_hisp_w22_g02.service.Interfaces;
 
 import com.example.be_java_hisp_w22_g02.dto.response.UserDto;
-import com.example.be_java_hisp_w22_g02.entity.User;
+import com.example.be_java_hisp_w22_g02.dto.response.UserFollowDTO;
 
 import java.util.List;
 
-import com.example.be_java_hisp_w22_g02.dto.response.UserFollowedDTO;
 
 public interface IUserService {
-    UserFollowedDTO getFollowedUsersById(Long id);
+    UserFollowDTO getFollowedUsersById(Integer id);
 
     void followUser(int userId, int userIdToFollow);
 
