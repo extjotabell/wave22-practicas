a4ee97af552eb77e5d131f639504dd5ea2624373
juliojalvare <juliojoel.alvarez@mercadolibre.com>
2023-09-15 11:36:52 -0300

    Add `getFollowedUsersById` user service method

--- 4. sprint I/be_java_hisp_w22_g02/src/main/java/com/example/be_java_hisp_w22_g02/service/Interfaces/IUserService.java
+++ 4. sprint I/be_java_hisp_w22_g02/src/main/java/com/example/be_java_hisp_w22_g02/service/Interfaces/IUserService.java
@@ -1,4 +1,7 @@
 package com.example.be_java_hisp_w22_g02.service.Interfaces;
 
+import com.example.be_java_hisp_w22_g02.dto.response.UserFollowedDTO;
+
 public interface IUserService {
+    UserFollowedDTO getFollowedUsersById(Long id);
 }
