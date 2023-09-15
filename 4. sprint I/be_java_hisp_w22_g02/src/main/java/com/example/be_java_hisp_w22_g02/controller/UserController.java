4cce13e73add938f17d64b706973a2f270106ce7
juliojalvare <juliojoel.alvarez@mercadolibre.com>
2023-09-15 12:56:18 -0300

    Add `getFollowedUser` controller

--- 4. sprint I/be_java_hisp_w22_g02/src/main/java/com/example/be_java_hisp_w22_g02/controller/UserController.java
+++ 4. sprint I/be_java_hisp_w22_g02/src/main/java/com/example/be_java_hisp_w22_g02/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.example.be_java_hisp_w22_g02.controller;
 
-import com.example.be_java_hisp_w22_g02.dto.response.UserFollowedDTO;
+import com.example.be_java_hisp_w22_g02.dto.response.UserFollowDTO;
 import com.example.be_java_hisp_w22_g02.service.Interfaces.IUserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatusCode;
@@ -8,12 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.be_java_hisp_w22_g02.service.Interfaces.IUserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -22,16 +17,15 @@ public class UserController {
 
     private final IUserService userService;
 
-    @GetMapping("/users/{userId}/followed/list")
-    public ResponseEntity<UserFollowedDTO> getFollowedUser(@PathVariable Long userId) {
-        UserFollowedDTO userFollowDTO = userService.getFollowedUsersById(userId);
-        return new ResponseEntity<>(userFollowDTO, HttpStatusCode.valueOf(200));
-    }
-
-
     @PostMapping("/users/{userId}/follow/{userIdToFollow}")
     public ResponseEntity<?> followUser(@PathVariable int userId, @PathVariable int userIdToFollow) {
         userService.followUser(userId, userIdToFollow);
         return ResponseEntity.ok("User followed successfully");
+    }
+
+    @GetMapping("/users/{userId}/followed/list")
+    public ResponseEntity<UserFollowDTO> getFollowedUser(@PathVariable Integer userId) {
+        UserFollowDTO userFollowDTO = userService.getFollowedUsersById(userId);
+        return new ResponseEntity<>(userFollowDTO, HttpStatusCode.valueOf(200));
     }
 }
