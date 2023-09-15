12d9dd3fa6c6ea06c2eca0a044e883f6217f7264
juliojalvare <juliojoel.alvarez@mercadolibre.com>
2023-09-15 16:44:41 -0300

    Refactor UserDto

--- 4. sprint I/be_java_hisp_w22_g02/src/main/java/com/example/be_java_hisp_w22_g02/dto/response/UserDto.java
+++ 4. sprint I/be_java_hisp_w22_g02/src/main/java/com/example/be_java_hisp_w22_g02/dto/response/UserDto.java
@@ -1,13 +1,28 @@
 package com.example.be_java_hisp_w22_g02.dto.response;
 
+import com.example.be_java_hisp_w22_g02.entity.Post;
+import com.example.be_java_hisp_w22_g02.entity.User;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
+import java.util.ArrayList;
+import java.util.List;
+
 @NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@Data
 public class UserDto {
-    private Long userId;
+    @JsonAlias({"user_id"})
+    private int userId;
+
+    @JsonAlias({"user_name"})
     private String userName;
+
+    private List<User> followers = new ArrayList<>();
+    private List<User> followed = new ArrayList<>();
+    private List<Post> posts = new ArrayList<>();
 }
