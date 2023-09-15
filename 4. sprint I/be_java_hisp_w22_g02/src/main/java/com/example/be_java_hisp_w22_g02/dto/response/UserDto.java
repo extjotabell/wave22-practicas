fe3fae0b2f3c1822d2e01baf34bd34eb3ab15d78
GonzaMessina <gonzalo.messina@mercadolibre.com>
2023-09-15 16:43:15 -0300

    Fix pull error

--- 4. sprint I/be_java_hisp_w22_g02/src/main/java/com/example/be_java_hisp_w22_g02/dto/response/UserDto.java
+++ 4. sprint I/be_java_hisp_w22_g02/src/main/java/com/example/be_java_hisp_w22_g02/dto/response/UserDto.java
@@ -1,29 +1,13 @@
 package com.example.be_java_hisp_w22_g02.dto.response;
 
-import com.example.be_java_hisp_w22_g02.entity.Post;
-import com.example.be_java_hisp_w22_g02.entity.User;
-import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
 @Data
-public class UserDto {
-
-    @JsonAlias({"user_id"})
-    private int userId;
-
-    @JsonAlias({"user_name"})
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private Long userId;
     private String userName;
-
-    private List<User> followers = new ArrayList<>();
-    private List<User> followed = new ArrayList<>();
-    private List<Post> posts = new ArrayList<>();
 }
