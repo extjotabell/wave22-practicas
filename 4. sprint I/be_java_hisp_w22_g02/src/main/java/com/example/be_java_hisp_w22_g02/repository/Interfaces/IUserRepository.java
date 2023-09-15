5346ca76e5d3d11beb1894ace8436c95ab28963c
juliojalvare <juliojoel.alvarez@mercadolibre.com>
2023-09-15 11:36:28 -0300

    Add `findById` user repository method

--- 4. sprint I/be_java_hisp_w22_g02/src/main/java/com/example/be_java_hisp_w22_g02/repository/Interfaces/IUserRepository.java
+++ 4. sprint I/be_java_hisp_w22_g02/src/main/java/com/example/be_java_hisp_w22_g02/repository/Interfaces/IUserRepository.java
@@ -1,5 +1,7 @@
 package com.example.be_java_hisp_w22_g02.repository.Interfaces;
 
-public interface IUserRepository {
+import com.example.be_java_hisp_w22_g02.entity.User;
 
+public interface IUserRepository {
+    User findById(Long id);
 }
