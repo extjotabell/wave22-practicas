ed4f0fa6f0a7f6584b3175cc5015c5a55c42fb48
gonzaloespanah <gonzalo.espana@mercadolibre.cl>
2023-09-15 10:20:00 -0300

    add method unfollow user in UserController

--- 4. sprint I/be_java_hisp_w22_g01/src/main/java/com/meli/be_java_hisp_w22_g01/controller/UserController.java
+++ 4. sprint I/be_java_hisp_w22_g01/src/main/java/com/meli/be_java_hisp_w22_g01/controller/UserController.java
@@ -41,4 +41,9 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(userService.getUserFollowedList(userId));
     }
 
+    @PostMapping("{userId}/unfollow/{userIdToUnfollow}")
+    public ResponseEntity<?> unfollowUser(@PathVariable int userId, @PathVariable int userIdToUnfollow){
+        return ResponseEntity.status(HttpStatus.OK).body(userService.unfollow(userId, userIdToUnfollow));
+    }
+
 }
