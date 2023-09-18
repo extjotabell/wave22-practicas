1935efb09262b77108d3d6b7412a5db3c35e7576
telorza <tatiana.elorza@mercadolibre.com>
2023-09-18 15:23:58 -0300

    add proyecto socialMeli parte grupal

---
+++ 4. sprint I/be_java_hisp_w22_g4/src/main/java/bootcamp/socialMeli/BeJavaHispW22G4Application.java
@@ -0,0 +1,14 @@
+package bootcamp.socialMeli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Properties;
+
+@SpringBootApplication
+public class BeJavaHispW22G4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeJavaHispW22G4Application.class, args);
+	}
+}
