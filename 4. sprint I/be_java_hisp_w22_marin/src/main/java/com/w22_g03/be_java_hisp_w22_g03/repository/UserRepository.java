0dc2a4981589bf821d17bca92f20ffff8b0ba0f0
matiMarin29 <matias.marin@mercadolibre.com>
2023-09-18 12:45:01 -0300

    Add repository individual

---
+++ 4. sprint I/be_java_hisp_w22_marin/src/main/java/com/w22_g03/be_java_hisp_w22_g03/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.w22_g03.be_java_hisp_w22_g03.repository;
+
+import com.w22_g03.be_java_hisp_w22_g03.model.Post;
+import com.w22_g03.be_java_hisp_w22_g03.model.User;
+
+import java.util.List;
+
+public interface UserRepository {
+
+    User findById(long id);
+
+    List<User> findFollowers(int userId);
+
+    User findFollower(int userIdToFollow, int userId);
+
+    List<User> findFollowed(int userId);
+
+    Post savePost(Post post);
+
+    List<User> findAll();
+}
