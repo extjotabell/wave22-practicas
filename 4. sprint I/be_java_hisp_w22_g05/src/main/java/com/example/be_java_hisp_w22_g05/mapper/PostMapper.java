252ea2dc75ab52ee5bf6f877646117aef27d9abd
NahuelFilippa <nahuel.filippa@mercadolibre.com>
2023-09-18 18:03:47 -0300

    Added the complete group project. Also added the two new individual functionalities

---
+++ 4. sprint I/be_java_hisp_w22_g05/src/main/java/com/example/be_java_hisp_w22_g05/mapper/PostMapper.java
@@ -0,0 +1,35 @@
+package com.example.be_java_hisp_w22_g05.mapper;
+
+import com.example.be_java_hisp_w22_g05.dto.PostDto;
+import com.example.be_java_hisp_w22_g05.dto.ProductDto;
+import com.example.be_java_hisp_w22_g05.entity.Post;
+import com.example.be_java_hisp_w22_g05.entity.Product;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostMapper {
+
+    public PostDto toPostDTO(Post post){
+        return PostDto.builder()
+                .id(post.getId())
+                .userId(post.getUser().getId())
+                .date(post.getDate().toString())
+                .category(post.getCategory())
+                .product(this.toProductDTO(post.getProduct()))
+                .price(post.getPrice())
+                .hasPromo(post.getHasPromo())
+                .discount(post.getDiscount())
+                .build();
+    }
+
+    private ProductDto toProductDTO(Product product){
+        return ProductDto.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .type(product.getType())
+                .brand(product.getBrand())
+                .color(product.getColor())
+                .notes(product.getNotes())
+                .build();
+    }
+}
