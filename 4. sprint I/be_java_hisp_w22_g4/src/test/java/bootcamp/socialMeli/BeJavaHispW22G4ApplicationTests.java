527e373b0abf475fad119c09bd174b0be7d54f7e
alehetman <alejandro.hetman@mercadolibre.com>
2023-09-19 15:51:48 -0300

    Añadiendo proyecto Sprint 1 grupal

---
+++ 4. sprint I/be_java_hisp_w22_g4/src/test/java/bootcamp/socialMeli/BeJavaHispW22G4ApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp.socialMeli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeJavaHispW22G4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
