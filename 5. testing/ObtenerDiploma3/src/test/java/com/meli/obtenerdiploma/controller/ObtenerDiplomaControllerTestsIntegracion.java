09f18ba88f3374c471f8aaee365c8b3e655caa31
pumaleiva <maximiliano.leiva@mercadolibre.com>
2023-09-22 10:51:03 -0300

    Ejercicio - Test de Integracion - ObtenerDiploma

---
+++ 5. testing/ObtenerDiploma3/src/test/java/com/meli/obtenerdiploma/controller/ObtenerDiplomaControllerTestsIntegracion.java
@@ -0,0 +1,47 @@
+package com.meli.obtenerdiploma.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ObtenerDiplomaControllerTestsIntegracion {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void obtenerDiploma() throws Exception {
+
+        ObjectWriter mapper = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE, false).writer();
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/analyzeScores/{studentId}", 1))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.studentName").value("Juan"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.averageScore").value("7.333333333333333"));
+    }
+
+    @Test
+    void obtenerDiplomaNotFound() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/analyzeScores/{studentId}","5"))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("StudentNotFoundException"));
+    }
+
+}
---
+++ 5. testing/ObtenerDiploma3/src/test/java/com/meli/obtenerdiploma/controller/StudentControllerTestsIntegration.java
@@ -0,0 +1,101 @@
+package com.meli.obtenerdiploma.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.meli.obtenerdiploma.model.StudentDTO;
+import com.meli.obtenerdiploma.model.SubjectDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class StudentControllerTestsIntegration {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void registerValidStudent() throws Exception {
+        SubjectDTO subjectDTO = new SubjectDTO("Matematica",5D);
+        StudentDTO studentDTO = new StudentDTO(null, "Caupolican", null, null, List.of(subjectDTO));
+        ObjectWriter mapper = new ObjectMapper()
+                .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .writer();
+        String payload = mapper.writeValueAsString(studentDTO);
+        mockMvc.perform(MockMvcRequestBuilders.post("/student/registerStudent")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(payload))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void registerNotValidStudent() throws Exception {
+        SubjectDTO subjectDTO = new SubjectDTO("matematica",5D); //Materia empieza con minuscula
+        StudentDTO studentDTO = new StudentDTO(null, "Caupolican", null, null, List.of(subjectDTO));
+        ObjectWriter mapper = new ObjectMapper()
+                .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .writer();
+        String payload = mapper.writeValueAsString(studentDTO);
+        mockMvc.perform(MockMvcRequestBuilders.post("/student/registerStudent")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(payload))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.name").value("MethodArgumentNotValidException"));
+    }
+
+    @Test
+    void getStudent() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/student/getStudent/{id}",1))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.studentName").value("Juan"));
+    }
+
+    @Test
+    void getNotValidStudent() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/student/getStudent/{id}",-1))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("StudentNotFoundException"));
+    }
+
+    //@Test
+    //void modifyStudent() {
+//
+    //}
+
+    @Test
+    void removeStudent() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/student/removeStudent/{id}",1))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void listStudents() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/student/listStudents"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].studentName").value("Pedro"));
+    }
+}
