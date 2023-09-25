4a3ba62331c5466b9abbb5c6c70969c24e8cb92b
LucianaPm <luciana.pio@mercadolibre.com>
2023-09-25 16:53:12 -0300

    Add scaffold tests

---
+++ 6. sprint II/be_java_hisp_w22_g4/src/test/java/bootcamp/socialMeli/unitTests/userService/GetAllUsersTest.java
@@ -0,0 +1,23 @@
+package bootcamp.socialMeli.unitTests.userService;
+
+import bootcamp.socialMeli.repository.UserRepositoryImpl;
+import bootcamp.socialMeli.service.UserServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class GetAllUsersTest {
+
+    @Mock
+    private UserRepositoryImpl userRepository;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+    @Test
+    void getAllUsersTestOk(){}
+}
