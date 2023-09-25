cb89a1305e9338c90587dd2cf4c8d839b49fcd0f
rmbayarri <renzo.bayarri@mercadolibre.com>
2023-09-25 11:50:36 -0300

    Added: Initial Scaffolding

---
+++ 6. sprint II/be_java_hisp_w22_g03/src/main/java/com/w22_g03/be_java_hisp_w22_g03/exception/BadRequestException.java
@@ -0,0 +1,9 @@
+package com.w22_g03.be_java_hisp_w22_g03.exception;
+
+public class BadRequestException extends RuntimeException{
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
+
