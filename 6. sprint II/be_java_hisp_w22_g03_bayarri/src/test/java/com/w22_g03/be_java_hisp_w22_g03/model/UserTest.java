971b66826a156dba3e851165d73be0322c8ef26b
rmbayarri <renzo.bayarri@mercadolibre.com>
2023-09-26 15:52:07 -0300

    Added: Initial Scaffolding

---
+++ 6. sprint II/be_java_hisp_w22_g03_bayarri/src/test/java/com/w22_g03/be_java_hisp_w22_g03/model/UserTest.java
@@ -0,0 +1,46 @@
+package com.w22_g03.be_java_hisp_w22_g03.model;
+
+import com.w22_g03.be_java_hisp_w22_g03.util.TestUtilGenerator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UserTest {
+    @Test
+    public void testIsSeller() {
+        // arrange
+        User userWithPosts = TestUtilGenerator.createTestUserSeller(1, "Insua");
+        User userWithoutPosts = TestUtilGenerator.createTestUser(2, "Batalla");
+
+        // assert
+        assertTrue(userWithPosts.isSeller());
+        assertFalse(userWithoutPosts.isSeller());
+    }
+
+    @Test
+    public void testAddFollower() {
+        // arrange
+        User user = TestUtilGenerator.createTestUser(1, "Insua");
+        User follower = TestUtilGenerator.createTestUser(2, "Batalla");
+
+        // act
+        user.addFollower(follower);
+
+        // assert
+        assertTrue(user.getFollowers().contains(follower));
+    }
+
+    @Test
+    public void testAddFollowed() {
+        // arrange
+        User user = TestUtilGenerator.createTestUser(1, "Insua");
+        User followedUser = TestUtilGenerator.createTestUser(2, "Batalla");
+
+        // act
+        user.addFollowed(followedUser);
+
+        // assert
+        assertTrue(user.getFollowed().contains(followedUser));
+    }
+}
