72a2c21dce47cb75559daa7f2215db8b297c27ff
Agustin Tarda <atarda@ar000hl6hwtg2m1.lan>
2023-09-26 08:57:31 -0300

    Added test for getFollowedUsersPostsById Ascending and Descending when there are no posts

--- 6. sprint II/be_java_hisp_w22_g03/src/test/java/com/w22_g03/be_java_hisp_w22_g03/service/PostServiceImplTest.java
+++ 6. sprint II/be_java_hisp_w22_g03/src/test/java/com/w22_g03/be_java_hisp_w22_g03/service/PostServiceImplTest.java
@@ -1,15 +1,35 @@
 package com.w22_g03.be_java_hisp_w22_g03.service;
 
+import com.w22_g03.be_java_hisp_w22_g03.dto.UserFollowedSellersPostsDTO;
+import com.w22_g03.be_java_hisp_w22_g03.model.User;
+import com.w22_g03.be_java_hisp_w22_g03.repository.PostRepository;
+import com.w22_g03.be_java_hisp_w22_g03.repository.PostRepositoryImpl;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class PostServiceImplTest {
 
+    @Mock
+    private PostRepository postRepository;
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private PostServiceImpl postService;
+
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
     @AfterEach
@@ -21,7 +41,23 @@ class PostServiceImplTest {
     }
 
     @Test
-    void getFollowedUsersPostsById() {
+    void getFollowedUsersPostsByIdWithNoPostsWithDescendingOrderReturnEmptyListOk() {
+        long userId = 1L;
+        when(userService.getUserById(userId)).thenReturn(new User(userId, "user1", new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
+
+        UserFollowedSellersPostsDTO userFollowedSellersPostsDTO = postService.getFollowedUsersPostsById(userId, "date_desc");
+
+        assertEquals(0, userFollowedSellersPostsDTO.getPosts().size());
+    }
+
+    @Test
+    void getFollowedUsersPostsByIdWithNoPostsWithAscendingOrderReturnEmptyListOk() {
+        long userId = 1L;
+        when(userService.getUserById(userId)).thenReturn(new User(userId, "user1", new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
+
+        UserFollowedSellersPostsDTO userFollowedSellersPostsDTO = postService.getFollowedUsersPostsById(userId, "date_asc");
+
+        assertEquals(0, userFollowedSellersPostsDTO.getPosts().size());
     }
 
     @Test
