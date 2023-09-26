c28acd951759cf28bc33b3b22595d731d2f784ae
pumaleiva <maximiliano.leiva@mercadolibre.com>
2023-09-26 11:35:34 -0300

    Added new Feature. T-0006 change LocalDate

--- 6. sprint II/be_java_hisp_w22_g05/src/test/java/com/example/be_java_hisp_w22_g05/Datos.java
+++ 6. sprint II/be_java_hisp_w22_g05/src/test/java/com/example/be_java_hisp_w22_g05/Datos.java
@@ -24,21 +24,27 @@ public class Datos {
 
     public static List<PostDto> getPostDtoListDesc(){
         List<PostDto> expected = new ArrayList<>();
+
         expected.add(new PostDto(1,2, LocalDate.now().minusDays(3), new ProductDto(2,"Silla Gamer","Gamer","Racer", "Red and Black", "Special Edition" ), 100, 1500.0, null, null));
         expected.add(new PostDto(2,2,LocalDate.now().minusDays(4), new ProductDto(1,"Silla Gamer II","Gamer","Racer", "Blue and Black", "Special Edition" ), 101, 1700.0, null, null));
+
         return expected;
     }
 
     public static List<PostDto> getPostDtoListAsc(){
         List<PostDto> expected = new ArrayList<>();
+
         expected.add(new PostDto(2,2,LocalDate.now().minusDays(4), new ProductDto(1,"Silla Gamer II","Gamer","Racer", "Blue and Black", "Special Edition" ), 101, 1700.0, null, null));
         expected.add(new PostDto(1,2, LocalDate.now().minusDays(3), new ProductDto(2,"Silla Gamer","Gamer","Racer", "Red and Black", "Special Edition" ), 100, 1500.0, null, null));
+
         return expected;
     }
     public static List<Post> getPostList(User user){
         List<Post> postList = new ArrayList<>();
+
         Post post1 = new Post(1, LocalDate.now().minusDays(3),100,user,new Product(2,"Silla Gamer","Gamer","Racer", "Red and Black", "Special Edition" ),1500.0,null,null);
         Post post2 = new Post(2, LocalDate.now().minusDays(4),101,user,new Product(1,"Silla Gamer II","Gamer","Racer", "Blue and Black", "Special Edition" ),1700.0,null,null);
+
         postList.add(post1);
         postList.add(post2);
         return postList;
