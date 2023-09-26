e05e495ad83677c946b3a80d4c7d6dfc74c1065d
NahuelFilippa <nahuel.filippa@mercadolibre.com>
2023-09-26 10:55:12 -0300

    Added test for US0006

--- 6. sprint II/be_java_hisp_w22_g05/src/test/java/com/example/be_java_hisp_w22_g05/service/PostServiceTest.java
+++ 6. sprint II/be_java_hisp_w22_g05/src/test/java/com/example/be_java_hisp_w22_g05/service/PostServiceTest.java
@@ -1,6 +1,5 @@
 package com.example.be_java_hisp_w22_g05.service;
 
-
 import com.example.be_java_hisp_w22_g05.dto.PostDto;
 import com.example.be_java_hisp_w22_g05.entity.Post;
 import com.example.be_java_hisp_w22_g05.entity.User;
@@ -14,6 +13,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import com.example.be_java_hisp_w22_g05.Datos;
+import com.example.be_java_hisp_w22_g05.dto.ProductDto;
+import com.example.be_java_hisp_w22_g05.entity.Product;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -21,13 +22,12 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -133,4 +133,43 @@ class PostServiceTest {
 
     }
 
+    @Test
+    void getListPostsFromSellersFollowedOkTest(){
+        //Arrange
+        List<Post> postList = new ArrayList<>();
+        User carlos = new User(1,"carlos",new ArrayList<>(),new ArrayList<>());
+        User maria = new User(2,"maria",new ArrayList<>(),new ArrayList<>());
+
+        carlos.getFollower().add(maria);
+        maria.getFollowed().add(carlos);
+
+        Product product1 = new Product(1, "arroz", "comida", "Gallo", "blanco", "");
+        Product product2 = new Product(2, "mouse", "IT", "Logitech", "Negro", "");
+
+        Post post1 = new Post(1, LocalDate.now().minusDays(13), 1, carlos, product1, 200.0, null, null);
+        Post post2 = new Post(2, LocalDate.now().minusDays(14), 2, carlos, product2, 1200.0, null, null);
+        Post post3 = new Post(3, LocalDate.now().minusDays(15), 3, carlos, product2, 2500.0, null, null);
+
+        postList.add(post1);
+        postList.add(post2);
+        postList.add(post3);
+
+        ProductDto productDto1 = new ProductDto(1, "arroz", "comida", "Gallo", "blanco", "");
+        ProductDto productDto2 = new ProductDto(2, "mouse", "IT", "Logitech", "Negro", "");
+
+        List<PostDto> postDtoList = new ArrayList<>();
+        postDtoList.add(new PostDto(1, carlos.getId(), post1.getDate(),
+                productDto1, post1.getCategory(), post1.getPrice(), post1.getHasPromo(),
+                post1.getDiscount()));
+
+        postDtoList.add(new PostDto(2, carlos.getId(), post2.getDate(),
+                productDto2, post2.getCategory(), post2.getPrice(), post2.getHasPromo(),
+                post2.getDiscount()));
+
+        when(postRepository.findPostAll()).thenReturn(postList);
+        when(userRepository.findUsersById(2)).thenReturn(maria);
+        //Act & Assert
+        Assertions.assertEquals(postDtoList,
+                postService.getListPostsFromSellersFollowed(2, "date_desc"));
+    }
 }
