03348a6448d43a7f045c17d089ed9fb65f160efb
EmmaSchenoni <emmanuel.schenoni@mercadolibre.com>
2023-09-26 15:26:16 -0300

    added set up test template

---
+++ 6. sprint II/be_java_hisp_w22_g02/src/test/java/com/example/be_java_hisp_w22_g02/repository/Implementations/UserRepositoryImplTest.java
@@ -0,0 +1,38 @@
+package com.example.be_java_hisp_w22_g02.repository.Implementations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserRepositoryImplTest {
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void followUser() {
+    }
+
+    @Test
+    void getFollowedPostLasTwoWeeks() {
+    }
+
+    @Test
+    void getFollowedPostLasTwoWeeksOrd() {
+    }
+
+    @Test
+    void existingUserById() {
+    }
+
+    @Test
+    void unFollowUser() {
+    }
+
+    @Test
+    void addPostToUser() {
+    }
+}
---
+++ 6. sprint II/be_java_hisp_w22_g02/src/test/java/com/example/be_java_hisp_w22_g02/service/Implementations/UserServiceImplTest.java
@@ -0,0 +1,50 @@
+package com.example.be_java_hisp_w22_g02.service.Implementations;
+
+import com.example.be_java_hisp_w22_g02.repository.Interfaces.IUserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+class UserServiceImplTest {
+
+    @Mock
+    IUserRepository userRepository;
+
+    @InjectMocks
+    UserServiceImpl userService;
+
+    @Test
+    void followUser() {
+    }
+
+    @Test
+    void getFollowers() {
+    }
+
+    @Test
+    void getTotalFollowersByUserId() {
+    }
+
+    @Test
+    void unfollowUser() {
+    }
+
+    @Test
+    void getFollowed() {
+    }
+
+    @Test
+    void addUserPost() {
+    }
+
+    @Test
+    void getLastTwoWeeksPostByUser() {
+    }
+}
