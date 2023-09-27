d41e03a473a1b2346975ce5a2f4d3fa424ff3163
Rodrigo Baliani <rodrigo.baliani@mercadolibre.com>
2023-09-27 12:58:11 -0300

    T0003 - Validate OK ascending alphabetic order - getFollowers

--- 6. sprint II/be_java_hisp_w22_g02/src/test/java/com/example/be_java_hisp_w22_g02/service/Implementations/UserServiceImplTest.java
+++ 6. sprint II/be_java_hisp_w22_g02/src/test/java/com/example/be_java_hisp_w22_g02/service/Implementations/UserServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.example.be_java_hisp_w22_g02.service.Implementations;
 
+import com.example.be_java_hisp_w22_g02.entity.User;
 import com.example.be_java_hisp_w22_g02.repository.Interfaces.IUserRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -8,7 +10,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
@@ -29,7 +32,19 @@ class UserServiceImplTest {
     }
 
     @Test
-    void getTotalFollowersByUserId() {
+    @DisplayName("T0003 - Validate OK ascending alphabetic order - getFollowers")
+    void getFollowers_OrderByNameAsc_ExistsTest() {
+
+        //Arrange
+        when(userRepository.existingUserById(anyInt())).thenReturn(true);
+        when(userRepository.findById(anyInt())).thenReturn(new User());
+
+        //Act
+        userService.getFollowers(1, "name_asc");
+
+        //Assert
+        verify(userRepository, atLeastOnce()).findById(anyInt());
+
     }
 
     @Test
