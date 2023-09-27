b2fca288adef26078667d540d31133c37b439eef
CeciMorlacchi <cecilia.morlacchi@mercadolibre.com>
2023-09-27 18:04:11 -0300

    Trabajo grupal sprint2

---
+++ 6. sprint II/be_java_hisp_w22_g05/src/main/java/com/example/be_java_hisp_w22_g05/dto/UserDto.java
@@ -0,0 +1,16 @@
+package com.example.be_java_hisp_w22_g05.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDto {
+    @JsonProperty("user_id")
+    private int id;
+    @JsonProperty("user_name")
+    private String name;
+}
