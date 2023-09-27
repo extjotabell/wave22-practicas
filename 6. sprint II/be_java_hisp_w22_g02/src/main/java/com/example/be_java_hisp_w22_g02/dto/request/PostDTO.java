863856217d3b24d9e4cc2925dbac66cf498a8b6e
juliojalvare <juliojoel.alvarez@mercadolibre.com>
2023-09-27 10:51:37 -0300

    Add Builder annotation to Post

--- 6. sprint II/be_java_hisp_w22_g02/src/main/java/com/example/be_java_hisp_w22_g02/dto/request/PostDTO.java
+++ 6. sprint II/be_java_hisp_w22_g02/src/main/java/com/example/be_java_hisp_w22_g02/dto/request/PostDTO.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class PostDTO {
 
     @JsonAlias({"post_id"})
