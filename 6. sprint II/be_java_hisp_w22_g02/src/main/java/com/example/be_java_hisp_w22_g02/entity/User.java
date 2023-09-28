19bbf33374aea9b016beda0b431b6a5a52571254
juliojalvare <juliojoel.alvarez@mercadolibre.com>
2023-09-28 11:21:41 -0300

    Add groupal project

---
+++ 6. sprint II/be_java_hisp_w22_g02/src/main/java/com/example/be_java_hisp_w22_g02/entity/User.java
@@ -0,0 +1,24 @@
+package com.example.be_java_hisp_w22_g02.entity;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class User {
+    @JsonAlias({"user_id"})
+    private int userId;
+
+    @JsonAlias({"user_name"})
+    private String userName;
+
+    private List<Integer> followers = new ArrayList<>();
+    private List<Integer> followed = new ArrayList<>();
+    private List<Post> posts = new ArrayList<>();
+
+}
