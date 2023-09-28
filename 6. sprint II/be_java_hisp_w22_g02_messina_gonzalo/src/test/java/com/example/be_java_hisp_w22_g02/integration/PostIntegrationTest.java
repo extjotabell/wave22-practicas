eae4aecfc0d9e25253c711c01125b80e8d8e0048
GonzaMessina <gonzalo.messina@mercadolibre.com>
2023-09-28 13:01:09 -0300

    Create integration test to project

---
+++ 6. sprint II/be_java_hisp_w22_g02_messina_gonzalo/src/test/java/com/example/be_java_hisp_w22_g02/integration/PostIntegrationTest.java
@@ -0,0 +1,59 @@
+package com.example.be_java_hisp_w22_g02.integration;
+
+import com.example.be_java_hisp_w22_g02.dto.request.PostDTO;
+import com.example.be_java_hisp_w22_g02.dto.request.ProductDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import net.bytebuddy.asm.Advice;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PostIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void getLastTwoWeeksPostByUserFollowed() throws Exception{
+
+        MvcResult mvcResult = mockMvc.perform(get("/products/followed/1/list"))
+                .andDo(print())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    public void addNewPost() throws Exception{
+        LocalDate  date = LocalDate.parse("2022-11-10");
+        ProductDto productDto = new ProductDto(1,"Laptop","Tecnologia","HP","Negro","Procesador Intel Core i5, 8GB RAM, 256GB SSD");
+        PostDTO postDTO = new PostDTO(1,1,date,productDto,1,1000);
+
+        ObjectWriter writer = new ObjectMapper().registerModule(new JavaTimeModule()).configure(SerializationFeature.WRAP_ROOT_VALUE,false).writer();
+        String payloadJson = writer.writeValueAsString(postDTO);
+
+        MvcResult mvcResult = mockMvc.perform(post("/products/post").contentType(MediaType.APPLICATION_JSON).content(payloadJson))
+                .andDo(print())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+}
---
+++ 6. sprint II/be_java_hisp_w22_g02_messina_gonzalo/src/test/java/com/example/be_java_hisp_w22_g02/integration/UserIntegrationTest.java
@@ -0,0 +1,78 @@
+package com.example.be_java_hisp_w22_g02.integration;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DisplayName(value = "IT - Follow user successfully")
+    public void followUser() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/users/1/follow/2"))
+                .andDo(print())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+    }
+
+
+    @Test
+    public void followerList() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/users/1/followers/list"))
+                .andDo(print())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userId").value(1))
+                .andReturn();
+
+    }
+
+    @Test
+    public void followedList() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/users/1/followed/list"))
+                .andDo(print())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userId").value(1))
+                .andReturn();
+
+    }
+
+    @Test
+    public void followersCount() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/users/1/followers/count"))
+                .andDo(print())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userId").value(1))
+                .andReturn();
+
+    }
+
+    @Test
+    public void unfollowUser() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/users/1/unfollow/2"))
+                .andDo(print())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userId").value(1))
+                .andReturn();
+
+    }
+}
