ff45826c244913a487206eaf604ecb5422a835c9
CeciMorlacchi <cecilia.morlacchi@mercadolibre.com>
2023-09-28 12:08:45 -0300

    Add test de integracion post

--- 6. sprint II/be_java_hisp_w22_g05_morlacchi/src/test/java/com/example/be_java_hisp_w22_g05/integracion/TestDeIntegracionPost.java
+++ 6. sprint II/be_java_hisp_w22_g05_morlacchi/src/test/java/com/example/be_java_hisp_w22_g05/integracion/TestDeIntegracionPost.java
@@ -15,28 +15,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.time.Clock;
-import java.time.Instant;
+
 import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.List;
+
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class TestDeIntegracionPost {
+
+    @Autowired
+    MockMvc mockMvc;
+
     static PostDto post;
     @BeforeAll
     static void setup(){
         ProductDto product = new ProductDto(5,"Silla","Gamer","Racer","Red","Special");
         post = new PostDto(2,1,LocalDate.now(),product,100,1000.00,true,0.25);
     }
-    @Autowired
-    MockMvc mockMvc;
 
     ObjectWriter mapper = new ObjectMapper()
             .registerModule(new JavaTimeModule())
@@ -58,18 +57,4 @@ public class TestDeIntegracionPost {
                 .andExpect(status().isOk());
     }
 
-    @Test
-    @DisplayName("US0006: getListPostsFromSellersFollowed")
-    void getListPostsFromSellersFollowed() throws Exception{
-        //ARRANGER
-        List<PostDto> expected = List.of(post);
-        String result = mapper.writeValueAsString(expected);
-
-        //ACT & ASSERTION
-        mockMvc.perform(post("/products/followed/{userId}/list",1))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json(result));
-    }
 }
