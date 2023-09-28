c9bb29cf5d5a962364c93e1683de53abdc230815
juliojalvare <juliojoel.alvarez@mercadolibre.com>
2023-09-28 12:26:46 -0300

    Add personal project

---
+++ 6. sprint II/be_java_hisp_w22_g02_alvarez/src/main/java/com/example/be_java_hisp_w22_g02/dto/response/TwoWeeksPostDTO.java
@@ -0,0 +1,16 @@
+package com.example.be_java_hisp_w22_g02.dto.response;
+
+import com.example.be_java_hisp_w22_g02.dto.request.PostDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TwoWeeksPostDTO {
+    private int userId;
+    private List<PostDTO> posts;
+}
---
+++ 6. sprint II/be_java_hisp_w22_g02_alvarez/src/test/java/com/example/be_java_hisp_w22_g02/controllers/PostControllerIT.java
@@ -0,0 +1,80 @@
+package com.example.be_java_hisp_w22_g02.controllers;
+
+import com.example.be_java_hisp_w22_g02.dto.request.PostDTO;
+import com.example.be_java_hisp_w22_g02.dto.request.ProductDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PostControllerIT {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    ObjectWriter writer = new ObjectMapper()
+            .registerModule(new JavaTimeModule())
+            .setDateFormat(new SimpleDateFormat("dd-MM-yyyy"))
+            .writer();
+
+    //Add user post
+    @Nested
+    class PostTest {
+
+        @Test
+        void shouldReturnPostWhenAddUserPost() throws Exception {
+            // ARRANGE
+            ProductDto productDto = ProductDto.builder()
+                    .productId(1)
+                    .productName("Remera")
+                    .type("Ropa")
+                    .brand("Lacoste")
+                    .color("Azul")
+                    .notes("Ninguna")
+                    .build();
+
+            PostDTO postDTO = PostDTO.builder()
+                    .postId(1)
+                    .userId(1)
+                    .date(LocalDate.parse("2023-09-27"))
+                    .product(productDto)
+                    .category(1)
+                    .price(10.0)
+                    .build();
+
+            // ACT
+            MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post("/products/post")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(writer.writeValueAsString(postDTO));
+
+            ResultMatcher statusExpected = MockMvcResultMatchers.status().isOk();
+            ResultMatcher contentTypeExpected = MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON);
+            ResultMatcher bodyExpected = MockMvcResultMatchers.content().json(writer.writeValueAsString(postDTO));
+
+            // ASSERT
+            mockMvc.perform(request)
+                    .andExpect(statusExpected)
+                    .andExpect(contentTypeExpected)
+                    .andExpect(bodyExpected)
+                    .andDo(MockMvcResultHandlers.print());
+
+        }
+    }
+}
