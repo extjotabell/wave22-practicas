54363517ccf94cda55852f8583743b6f2c7ae3c2
magui-cagna <mariaagostina.cagna@mercadolibre.com>
2023-09-28 12:24:15 -0400

    Adding small changes to Validations that were made for the group project

--- 6. sprint II/be_java_hisp_w22_g02_cagna/src/main/java/com/example/be_java_hisp_w22_g02/dto/request/PostDTO.java
+++ 6. sprint II/be_java_hisp_w22_g02_cagna/src/main/java/com/example/be_java_hisp_w22_g02/dto/request/PostDTO.java
@@ -2,6 +2,7 @@ package com.example.be_java_hisp_w22_g02.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,25 +22,24 @@ public class PostDTO {
     private Integer postId;
 
     @Min(value = 1, message = "User id can't be less than 1")
-    @Max(100)
     @JsonAlias({"user_id"})
     @NotNull(message = "User id can't be blank/empty")
     private Integer userId;
 
-    @NotNull
+    @NotNull(message = "Date on a post can't be null")
     @JsonFormat(pattern = "dd-MM-yyyy")
     private LocalDate date;
 
-    @NotNull
+    @NotNull(message = "Product can't be null on a post")
+    @Valid
     private ProductDto product;
 
-    @Min(1)
-    @Max(100)
+    @Min(value = 1, message = "Category cant be less than 1")
     @NotNull(message = "Category can't be blank or empty")
     private int category;
 
-    @Min(1)
-    @Max(10000000)
+    @Min(value = 1, message = "Price cant be less than 1")
+    @Max(value = 10000000, message = "Price cant be higher than 10000000")
     @NotNull(message = "Price can't be blank or empty")
     private double price;
 
