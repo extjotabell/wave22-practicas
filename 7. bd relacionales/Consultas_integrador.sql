e18ff3d3a8b49f7def4e68622bbfe7ddf257bbcf
CeciMorlacchi <cecilia.morlacchi@mercadolibre.com>
2023-09-29 17:52:30 -0300

    Ejercicicios

---
+++ 7. bd relacionales/Consultas_integrador.sql
@@ -0,0 +1,19 @@
+insert into clientes values (1,"12345678", "Carlos", "Perez", "1987-03-12","Santa Fe", "Rafaela",1);
+insert into clientes values (2,"12345672", "Juan", "Jimenez", "1987-03-12","Buenos Aires", "CABA",2);
+insert into clientes values (3,"12345673", "Maria", "Garcia", "1987-03-12","Cordoba", "Marcos Juarez",3);
+insert into clientes values (4,"12345674", "Agustina", "Rodriguez", "1987-03-12","Buenos Aires", "Vicente Lopez",4);
+insert into clientes values (5,"12345675", "Eduardo", "Sanchez", "1987-03-12","Mendoza", "Mendoza",5);
+insert into clientes values (6,"12345676", "Ivan", "Alient", "1987-03-12","Salta", "Cafayate",1);
+insert into clientes values (7,"12345677", "Emma", "Mitch", "1987-03-12","Jujuy", "Jujuy",2);
+insert into clientes values (8,"12345679", "Carla", "Ahhg", "1987-03-12","San Luis", "Merlo",3);
+insert into clientes values (9,"12345671", "Josefina", "Nhdju", "1987-03-12","Tierra del Fuego", "Rio Grande",4);
+insert into clientes values (10,"12345698", "Claudio", "Gfstuj", "1987-03-12","Santa Fe", "Sunchales",5);
+
+insert into planes values(1,100,1000,0.10);
+insert into planes values(2,50,250,0.10);
+insert into planes values(3,300,750,0.10);
+insert into planes values(4,1000,2000,0.10);
+insert into planes values(5,150,350,0.10);
+
+select * from planes;
+select * from clientes;
