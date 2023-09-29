6984694ec387c8b012479f37cfee5c430fd9e75f
facujaneiro <facundo.janeiro@mercadolibre.com>
2023-09-29 17:15:27 -0300

    agrego ejercicios

---
+++ 7. bd relacionales/EjercicioConsultasMovies/ejercicio1vivo.sql
@@ -0,0 +1,27 @@
+/*select * from actors*/
+
+/*select first_name,last_name,rating from actors*/
+
+/*select title as titulo from series*/
+
+/*select first_name,last_name from actors where rating > 7.5*/
+
+/*select title,rating,awards from movies where rating >= 7.5 and awards > 2*/
+
+/*select title,rating from movies order by rating ASC*/
+
+/*select title from movies limit 3*/
+
+/*select title,rating from movies order by rating DESC limit 5*/
+
+/*select * from actors limit 10*/
+
+/*select title,rating from movies where title like 'Toy Story%'*/
+
+/*select first_name,last_name from actors where first_name like 'Sam%'*/
+
+/*select title, release_date from movies where release_date between '2004-01-01' and '2008-01-01'*/
+
+/*select title as titulo, rating as calificacion, release_date as fecha from movies where rating > 3 
+and release_date between '1988-01-01' and '2009-01-01'
+order by rating desc*/
