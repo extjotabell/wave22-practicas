bdc7c6543274c1e5a9aa534f8eb207a36f2f52f9
rmbayarri <renzo.bayarri@mercadolibre.com>
2023-10-02 11:16:04 -0300

    Fix queries

--- 7. bd relacionales/Advance Querys Movies.sql
+++ 7. bd relacionales/Advance Querys Movies.sql
@@ -1,6 +1,6 @@
 # Mostrar el título y el nombre del género de todas las series.
 
-SELECT m.title, g.name FROM movies m INNER JOIN genres g ON m.genre_id = g.id;
+SELECT s.title, g.name FROM series s INNER JOIN genres g ON s.genre_id = g.id;
 
 # Mostrar el título de los episodios, el nombre y apellido de los actores que trabajan en cada uno de ellos.
 
@@ -21,12 +21,18 @@ SELECT g.name, count(m.id) as quantity
 FROM genres g
 INNER JOIN movies m ON g.id = m.genre_id
 GROUP BY g.name
-HAVING quantity > 3;
+HAVING quantity >= 3;
 
 # Mostrar sólo el nombre y apellido de los actores que trabajan en todas las películas de la guerra de las galaxias y que estos no se repitan.
 
-SELECT DISTINCT a.first_name, a.last_name
+SELECT a.first_name, a.last_name
 FROM actors a
 INNER JOIN actor_movie am ON a.id = am.actor_id
 INNER JOIN movies m ON am.movie_id = m.id
-WHERE m.title LIKE '%Guerra de las Galaxias%';
+WHERE m.title LIKE '%Guerra de las Galaxias%'
+GROUP BY a.id, a.first_name, a.last_name
+HAVING COUNT(m.id) = (
+  SELECT COUNT(*)
+  FROM movies
+  WHERE title LIKE '%Guerra de las Galaxias%'
+);
