479b5e955134d906be6de1bdc809051f78a4563b
Gonzalo Garcia Aguirre <gonzalo.garciaaguirre@mercadolibre.com>
2023-10-02 17:24:07 -0300

    Added sql for temp table & index / DER PG Async

---
+++ 7. bd relacionales/sql-avanzado-vivo/TPindex.sql
@@ -0,0 +1,12 @@
+create temporary table TWD(
+select e.title from episodes e
+inner join seasons sea on e.season_id = sea.id
+inner join series se on sea.serie_id = se.id
+where se.title like "The Walking Dead"
+);
+
+select * from TWD;
+
+create index series_index on series (release_date);
+
+explain select * from series where release_date = '2011-01-01 00:00:00';
