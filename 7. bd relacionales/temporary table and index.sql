5ac042e8fbcf774f11144d3ede871d58ba134770
Nicolás Salvo <nicolas.salvo@mercadolibre.com>
2023-10-02 17:16:57 -0300

    feat: done group exercise bills AND temporary table and index

---
+++ 7. bd relacionales/temporary table and index.sql
@@ -0,0 +1,22 @@
+DROP TEMPORARY TABLE IF EXISTS TWD;
+# Forma 1
+CREATE TEMPORARY TABLE TWD AS
+SELECT e.title AS "Episode Title", e.release_date, s.title AS "Season Title", s.number FROM episodes e
+JOIN seasons s ON e.season_id = s.id
+JOIN series se ON s.serie_id = se.id
+WHERE se.title LIKE "%The Walking Dead%";
+
+# Forma 2
+# CREATE TEMPORARY TABLE TWD (
+#
+# );
+# INSERT INTO TWD
+# SELECT e.title AS "Episode Title", e.release_date, s.title AS "Season Title", s.number FROM episodes e
+# JOIN seasons s ON e.season_id = s.id
+# JOIN series se ON s.serie_id = se.id
+# WHERE se.title LIKE "%The Walking Dead%";
+SELECT * FROM TWD WHERE number = 1;
+
+CREATE INDEX index_rating
+ON movies (rating);
+SHOW INDEX FROM movies;
