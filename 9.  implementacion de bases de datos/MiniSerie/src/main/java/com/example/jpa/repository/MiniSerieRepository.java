a7edf08ad1fd4efb20975ba84c1dc837f101d930
rmbayarri <renzo.bayarri@mercadolibre.com>
2023-10-05 16:14:42 -0300

    Added project Miniserie

---
+++ 9.  implementacion de bases de datos/MiniSerie/src/main/java/com/example/jpa/repository/MiniSerieRepository.java
@@ -0,0 +1,7 @@
+package com.example.jpa.repository;
+
+import com.example.jpa.model.MiniSerie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MiniSerieRepository extends JpaRepository<MiniSerie, Long> {
+}
