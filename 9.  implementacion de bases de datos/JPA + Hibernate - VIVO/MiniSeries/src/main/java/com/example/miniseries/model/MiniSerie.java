9a6f285aa0aeecf1536ca9e50d3f72993e60b2c1
Randy Mendoza <randy.mendoza@mercadolibre.com>
2023-10-05 17:25:52 -0300

    agregado de conexion con H2 y mysql

---
+++ 9.  implementacion de bases de datos/JPA + Hibernate - VIVO/MiniSeries/src/main/java/com/example/miniseries/model/MiniSerie.java
@@ -0,0 +1,25 @@
+package com.example.miniseries.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+public class MiniSerie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double rating;
+
+    @Column(name = "amount_of_awards")
+    private int amountOFAwards;
+
+    private int test;
+}
---
+++ 9.  implementacion de bases de datos/JPA + Hibernate - VIVO/MiniSeries/src/main/java/com/example/miniseries/repository/MiniSerieRepository.java
@@ -0,0 +1,7 @@
+package com.example.miniseries.repository;
+
+import com.example.miniseries.model.MiniSerie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MiniSerieRepository extends JpaRepository<MiniSerie, Long> {
+}
